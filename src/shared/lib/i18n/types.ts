4b14c67680d7e4e858abeedff21e23ce2e34bee8
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2025-09-06 19:00:10 +0400

    Fix

--- src/shared/lib/i18n/types.ts
+++ src/shared/lib/i18n/types.ts
@@ -3,7 +3,7 @@ import { AboutDict } from "./directories/about/types";
 import { ContactDict } from "./directories/contact/types";
 import { HomeDict } from "./directories/home/types";
 
-export type HeaderDict = CommonDict["header"];
+export type HeaderDict = CommonDict["nav"];
 
 export type Dict = {
   common: CommonDict;
