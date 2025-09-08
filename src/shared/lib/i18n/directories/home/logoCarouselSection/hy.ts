8f08aff124dfeaaae8d9f906c23daed2d7c69521
Tigran Ghumashyan <tigranghumashyan@gmail.com>
2025-09-09 02:58:33 +0400

    Fix

--- src/shared/lib/i18n/directories/home/logoCarouselSection/hy.ts
+++ src/shared/lib/i18n/directories/home/logoCarouselSection/hy.ts
@@ -1,6 +1,6 @@
 import type { LogoCarouselSectionDict } from "../types";
 
 export const logoCarouselSection: LogoCarouselSectionDict = {
-  title: "Լավագույնները արդեն այստեղ են",
+  title: "Մեր գործընկերները",
   subtitle: "Միացիր նոր մշակույթին",
 };
--- src/widgets/LogoCarouselSection/LogoCarouselSection.tsx
+++ src/widgets/LogoCarouselSection/LogoCarouselSection.tsx
@@ -4,95 +4,12 @@ import { Container, Section } from "@/shared/ui/custom";
 import { GradientHeading } from "@/shared/ui/lib";
 import { LogoCarousel } from "@/shared/ui/lib/logo-carousel";
 import React, { type SVGProps } from "react";
+import ArdaLogo from "../../../public/arda.jpg";
 
-function AppleIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      xmlns="http://www.w3.org/2000/svg"
-      width="209"
-      height="256"
-      viewBox="0 0 814 1000"
-      {...props}
-    >
-      <path d="M788.1 340.9c-5.8 4.5-108.2 62.2-108.2 190.5 0 148.4 130.3 200.9 134.2 202.2-.6 3.2-20.7 71.9-68.7 141.9-42.8 61.6-87.5 123.1-155.5 123.1s-85.5-39.5-164-39.5c-76.5 0-103.7 40.8-165.9 40.8s-105.6-57-155.5-127C46.7 790.7 0 663 0 541.8c0-194.4 126.4-297.5 250.8-297.5 66.1 0 121.2 43.4 162.7 43.4 39.5 0 101.1-46 176.3-46 28.5 0 130.9 2.6 198.3 99.2zm-234-181.5c31.1-36.9 53.1-88.1 53.1-139.3 0-7.1-.6-14.3-1.9-20.1-50.6 1.9-110.8 33.7-147.1 75.8-28.5 32.4-55.1 83.6-55.1 135.5 0 7.8 1.3 15.6 1.9 18.1 3.2.6 8.4 1.3 13.6 1.3 45.4 0 102.5-30.4 135.5-71.3z" />
-    </svg>
-  );
-}
-
-
-function VercelIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      viewBox="0 0 256 222"
-      width="256"
-      height="222"
-      xmlns="http://www.w3.org/2000/svg"
-      preserveAspectRatio="xMidYMid"
-      {...props}
-    >
-      <path fill="#000" d="m128 0 128 221.705H0z" />
-    </svg>
-  );
-}
-
-function SupabaseIcon(props: SVGProps<SVGSVGElement>) {
-  return (
-    <svg
-      viewBox="0 0 109 113"
-      width="109"
-      height="113"
-      fill="none"
-      xmlns="http://www.w3.org/2000/svg"
-      {...props}
-    >
-      <path
-        d="M63.708 110.284c-2.86 3.601-8.658 1.628-8.727-2.97l-1.007-67.251h45.22c8.19 0 12.758 9.46 7.665 15.874l-43.151 54.347Z"
-        fill="url(#a)"
-      />
-      <path
-        d="M63.708 110.284c-2.86 3.601-8.658 1.628-8.727-2.97l-1.007-67.251h45.22c8.19 0 12.758 9.46 7.665 15.874l-43.151 54.347Z"
-        fill="url(#b)"
-        fillOpacity=".2"
-      />
-      <path
-        d="M45.317 2.071c2.86-3.601 8.657-1.628 8.726 2.97l.442 67.251H9.83c-8.19 0-12.759-9.46-7.665-15.875L45.317 2.072Z"
-        fill="#3ECF8E"
-      />
-      <defs>
-        <linearGradient
-          id="a"
-          x1="53.974"
-          y1="54.974"
-          x2="94.163"
-          y2="71.829"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop stopColor="#249361" />
-          <stop offset="1" stopColor="#3ECF8E" />
-        </linearGradient>
-        <linearGradient
-          id="b"
-          x1="36.156"
-          y1="30.578"
-          x2="54.484"
-          y2="65.081"
-          gradientUnits="userSpaceOnUse"
-        >
-          <stop />
-          <stop offset="1" stopOpacity="0" />
-        </linearGradient>
-      </defs>
-    </svg>
-  );
-}
-
-// Массив с логотипами
 type LogoItem = { id: number; name: string; img: any };
 
 const allLogos: LogoItem[] = [
-  { name: "Apple", id: 1, img: AppleIcon },
-  { name: "Supabase", id: 2, img: SupabaseIcon },
-  { name: "Vercel", id: 3, img: VercelIcon },
+  { name: "ARDA", id: 1, img: ArdaLogo 
 ];
 
 export interface LogoCarouselSectionProps {
